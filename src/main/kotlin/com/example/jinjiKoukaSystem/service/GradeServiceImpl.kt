ac6413f5499391c170ede88850cc7d951a629e94
May_Thazin <may-thazin@honda-logi.com>
2021-08-03 08:12:06 +0630

    過去一覧(サービス追加)

---
+++ src/main/kotlin/com/example/jinjiKoukaSystem/service/GradeServiceImpl.kt
@@ -0,0 +1,18 @@
+package com.example.jinjiKoukaSystem.service
+
+import com.example.jinjiKoukaSystem.model.Grade
+import com.example.jinjiKoukaSystem.respository.GradeRepository
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Service
+
+@Service
+class GradeServiceImpl {
+    @Autowired
+    private val ListRepo: GradeRepository? = null
+
+    fun patternSearch(): List<Grade?>? {
+        return ListRepo?.appraisalPattern()
+    }
+
+
+}
