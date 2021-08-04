17410685589bd91f2087625291eef64aba80b995
Hsu_Yee_Hnin <hsuyeehnin.hyh@gmail.com>
2021-08-04 11:54:58 +0630

    2021.08.04 Commit

--- src/main/kotlin/com/example/jinjiKoukaSystem/repository/AuthorizerRegistrationRepository.kt
+++ src/main/kotlin/com/example/jinjiKoukaSystem/repository/AuthorizerRegistrationRepository.kt
@@ -1,12 +1,11 @@
 package com.example.jinjiKoukaSystem.repository
 
-import com.example.jinjiKoukaSystem.model.AuthorizerSetting
 import org.springframework.data.jpa.repository.Query
 import org.springframework.data.repository.CrudRepository
 import org.springframework.stereotype.Repository
 
 @Repository
-interface AuthorizerSettingRepository : CrudRepository<AuthorizerSetting, String> {
+interface AuthorizerRegistrationRepository : CrudRepository<AuthorizerSetting, String> {
     @Query(
         value = "SELECT * FROM m_base",
         nativeQuery = true
