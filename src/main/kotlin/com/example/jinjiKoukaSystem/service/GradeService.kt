f24b951a980449c0e4140e310b46bd215ddbf978
Hsu_Yee_Hnin <hsuyeehnin.hyh@gmail.com>
2021-08-05 08:51:28 +0630

    2021.08.04 Commit

--- src/main/kotlin/com/example/jinjiKoukaSystem/service/GradeService.kt
+++ src/main/kotlin/com/example/jinjiKoukaSystem/service/GradeService.kt
@@ -1,19 +1,13 @@
 package com.example.jinjiKoukaSystem.service
 
 //import com.example.jinjiKoukaSystem.model.Grade
+import com.example.jinjiKoukaSystem.model.Grade
 import org.springframework.data.domain.Page
 
 interface GradeService {
 
-//    @Throws(Exception::class)
-//    open fun deleteProject(pj: ProjectList?): ProjectList?
-//
-//    open fun updateProject(pj: ProjectList?): ProjectList
-//
-//    fun findPaginated(pageNo: Int, pageSize: Int): Page<ProjectList?>?
-//
-//    @Throws(java.lang.Exception::class)
-//    fun saveProject(projectList: ProjectList?): ProjectList?
+    @Throws(java.lang.Exception::class)
+    fun saveGrade(grade: Grade?): Grade?
 
 }
 
