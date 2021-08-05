329af9941d39d2da3847508eeabea7ecdfae754a
ThazinWai <36944218+ThazinWai@users.noreply.github.com>
2021-08-05 08:37:21 +0630

    Update HomePageController.kt

--- src/main/kotlin/com/example/jinjiKoukaSystem/controller/HomePageController.kt
+++ src/main/kotlin/com/example/jinjiKoukaSystem/controller/HomePageController.kt
@@ -5,7 +5,6 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.*
-import com.example.jinjiKoukaSystem.repository.GradeRepository
 import com.example.jinjiKoukaSystem.service.GradeServiceImpl
 
 @Controller
